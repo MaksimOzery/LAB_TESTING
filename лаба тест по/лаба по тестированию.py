02a3f86146cf3f9f0bd01a72e97ded914276c923
MaksimOzery <44927164+MaksimOzery@users.noreply.github.com>
2019-09-22 12:36:52 +0300

    Update лаба по тестированию.py

--- лаба тест по/лаба по тестированию.py
+++ лаба тест по/лаба по тестированию.py
@@ -6,11 +6,11 @@ driver = webdriver.Chrome('C:\chromedriver.exe')
 driver.get('http://justnotepad.com/ru/');
 time.sleep(5) 
 
-#--------------------Ввод данных в окно---------------------------------
+#--------------------input information window---------------------------------
 div=driver.find_element_by_id("draft_form")
 g=div.find_element_by_id("editable_text")
 g.send_keys(".,/\-=+0123456789*~@#$%^&*()_{}[]abvgdzxcewqrtyuiopljkmn ")
-#--------------Проверка нажатия отмены удаления------------------------------
+#--------------press button cancel------------------------------
 
 div2=driver.find_element_by_id("header")
 gk=div2.find_element_by_id("delete_draft")
@@ -20,7 +20,7 @@ div3=driver.find_element_by_id("confirm_lightbox")
 cancel=div3.find_element_by_id("cancel")
 cancel.click()
 time.sleep(3)
-#------------------------Нажатие удаления----------------
+#------------------------press button delete----------------
 div2=driver.find_element_by_id("header")
 gk=div2.find_element_by_id("delete_draft")
 gk.click()
@@ -29,7 +29,7 @@ div3=driver.find_element_by_id("confirm_lightbox")
 ok=gk=div3.find_element_by_id("ok")
 ok.click()
 
-#-------------------Очистка черновика----------------------
+#-------------------clears----------------------
 g=div.find_element_by_id("editable_text")
 g.send_keys(".,/\-=+0123456789*~@#$%^&*()_{}[]abvgdzxcewqrtyuiopljkmn  ")
 time.sleep(5)
@@ -43,16 +43,16 @@ div3=driver.find_element_by_id("confirm_lightbox")
 ok=gk=div3.find_element_by_id("ok")
 ok.click()
 
-#--------------------в ночном или дневном режиме---------------------
+#--------------------night and day---------------------
 style=driver.find_element_by_id("style_mode")
 style_ok=style.find_element_by_id("night_mode")
 style_ok.click()
-#---------------------------задержка------------------------- 
+#---------------------------delay------------------------- 
 time.sleep(1)
 style_ok=style.find_element_by_id("day_mode")
 style_ok.click()
 time.sleep(2)
-#---Проход по ссылкам------------------------------------------
+#---following links------------------------------------------
 
 
 s2=driver.find_element_by_id("footer_menu")
@@ -70,7 +70,7 @@ driver.find_element_by_css_selector("a[href='feedback/']").click()
 driver.get('http://justnotepad.com/ru/');
 time.sleep(3)
 
-#--------------Переход на англ версию----------------------------------
+#--------------English version----------------------------------
 
 
 s1=driver.find_element_by_id("footer_lang")
@@ -78,7 +78,7 @@ s1.click()
 time.sleep(1)
 
 
-#-----------------------переход на новое окно--------------------------
+#-----------------------open new Window--------------------------
 div2=driver.find_element_by_id("header")
 new=div2.find_element_by_id("new_draft")
 new.click()
@@ -86,7 +86,7 @@ new.click()
 
 
 
-time.sleep(5) # задержка и выход
+time.sleep(5) # close window
 driver.quit()
 
 
